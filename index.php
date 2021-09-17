513c29d355af4b175e5fe7b9ed0da36a5d7a780d
Giovany <giovanyandreso@gmail.com>
2021-09-17 11:06:13 -0500

    Subiendo a git 0.1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>beta</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+	<center>
+		<img src="img/cenigraf.png" >
+	</center>
+	
+
+</body>
+</html>
