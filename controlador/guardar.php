503498517cd774791e9c5f2eb5dce72d5f00fd11
Giovany <giovanyandreso@gmail.com>
2021-10-15 12:24:31 -0500

    Beta ---------------------------------------------------------------------- 1.0

--- controlador/guardar.php
+++ controlador/guardar.php
@@ -48,13 +48,32 @@ if ($hours==8) {
 }
 
 
+$hora_i=2;
+
+$querys="SELECT SUM(horas_instructor) as total FROM horarios WHERE horarios.instructor=$ins";
+$resuls=mysqli_query($conn,$querys);
+$row=mysqli_fetch_array($resuls);
+$sum=$row['total'];
 
 
 
-$query="INSERT INTO `horarios` (`id_hora`, `dia`, `ficha`, `instructor`, `hora`) VALUES (NULL,'$days','$fich','$ins','$hours');";
+if ($sum<40) {
+	$query="INSERT INTO `horarios` (`id_hora`, `dia`, `ficha`, `instructor`, `hora`,`horas_instructor`) VALUES (NULL,'$days','$fich','$ins','$hours','$hora_i');";
     mysqli_query($conn,$query);
+    $sum2=38-$sum;
+    echo "<script>
+                  alert('El instructor le faltan $sum2 horas por asignar.');
+                  window.location= '../vista/horarios_beta.php?instructor=$ins'
+              </script>";     
+    
+}elseif ($sum>=40) {
+	echo "<script>
+                  alert('El instructor completo sus $sum horas.');
+                  window.location= '../vista/horarios_beta.php?instructor=$ins'
+              </script>"; 
+   
+}
 
 
-
-header("location:../vista/horarios_beta.php?instructor=$ins");
+//header("location:../vista/horarios_beta.php?instructor=$ins");
 ?>
