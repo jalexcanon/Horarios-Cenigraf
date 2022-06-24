eb05384509d779ba0f6ddeacec764e6a0756713a
Alexa <jalex.canon@gmail.com>
2022-06-24 18:42:16 -0500

    footer

--- vista/parte_inferior.php
+++ vista/parte_inferior.php
@@ -7,7 +7,7 @@
 
 </div>
 <!-- jQuery-->
-
+<script src="../plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- jQuery UI -- >
